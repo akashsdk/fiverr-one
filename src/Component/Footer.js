90d2eeb9c20c4b74344311a26f454bbe9c67d276
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2023-12-26 02:11:49 +0600

    ok

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import "./Footer.css";
 
 import { Button } from "antd";
@@ -11,15 +11,52 @@ import {
 } from "@ant-design/icons";
 
 export default function Footer() {
+  const [showBox, setShowBox] = useState(true);
+
+  const handleClose = () => {
+    setShowBox(false);
+  };
+  const handleOpen = () => {
+    setShowBox(true);
+  };
+
+  const handleReload = () => {
+    setShowBox(true);
+  };
+
+  useEffect(() => {
+    window.addEventListener('beforeunload', handleReload);
+
+    return () => {
+      window.removeEventListener('beforeunload', handleReload);
+    };
+  }, []);
   return (
     <div className="footerBody">
+      {showBox && (
+        <div className="footer-showBox">
+          <p>This is a closeable box.</p>
+          <button onClick={handleClose}>Close</button>
+        </div>
+      )}
       <div className="footerBox1">Logo</div>
       <div className="footerBox2">
-      <Link className="footerBox2-Link" to="/" >Home</Link>
-      <Link className="footerBox2-Link" to="/About-Us" >About Us</Link>
-      <Link className="footerBox2-Link" to="/Contact" >Contact</Link>
-          
-        <Button className="footerBox2-Button" type="text" block>
+        <Link className="footerBox2-Link" to="/">
+          Home
+        </Link>
+        <Link className="footerBox2-Link" to="/About-Us">
+          About Us
+        </Link>
+        <Link className="footerBox2-Link" to="/Contact">
+          Contact
+        </Link>
+
+        <Button
+          className="footerBox2-Button"
+          onClick={handleOpen}
+          type="text"
+          block
+        >
           Portfolio
         </Button>
         <Button className="footerBox2-Button" type="text" block>
--- src/Screen/Home.js
+++ src/Screen/Home.js
@@ -3,36 +3,11 @@ import '../Style/Home.css';
 
 
 export default function Home() {
-  const [showBox, setShowBox] = useState(true);
-
-  const handleClose = () => {
-    setShowBox(false);
-  };
-  const handleOpen = () => {
-    setShowBox(true);
-  };
-
-  const handleReload = () => {
-    setShowBox(true);
-  };
-
-  useEffect(() => {
-    window.addEventListener('beforeunload', handleReload);
-
-    return () => {
-      window.removeEventListener('beforeunload', handleReload);
-    };
-  }, []);
+  
 
   return (
     <div className='homeBody'>
-        {showBox && (
-        <div style={{ border: '1px solid black', padding: '10px', marginBottom: '20px' }}>
-          <p>This is a closeable box.</p>
-          <button onClick={handleClose}>Close</button>
-        </div>
-      )}
-      <button onClick={handleOpen}>open</button>
+      <h1>home</h1>
     </div>
   )
 }
