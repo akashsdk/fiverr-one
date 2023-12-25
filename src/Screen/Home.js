6456f4c8da8793d20b433c38a710c7f1676b28f4
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2023-12-26 00:40:04 +0600

    ok

--- src/Screen/Home.js
+++ src/Screen/Home.js
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import '../Style/Home.css';
 
+
 export default function Home() {
+  const [showBox, setShowBox] = useState(true);
+
+  const handleClose = () => {
+    setShowBox(false);
+  };
+  const handleOpen = () => {
+    setShowBox(true);
+  };
+
+  const handleReload = () => {
+    setShowBox(true);
+  };
+
+  useEffect(() => {
+    window.addEventListener('beforeunload', handleReload);
+
+    return () => {
+      window.removeEventListener('beforeunload', handleReload);
+    };
+  }, []);
+
   return (
     <div className='homeBody'>
-        akash
+        {showBox && (
+        <div style={{ border: '1px solid black', padding: '10px', marginBottom: '20px' }}>
+          <p>This is a closeable box.</p>
+          <button onClick={handleClose}>Close</button>
+        </div>
+      )}
+      <button onClick={handleOpen}>open</button>
     </div>
   )
 }
