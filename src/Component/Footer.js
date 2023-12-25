c63f47964130c876502aa798cbbd88cb04119e03
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2023-12-26 01:13:24 +0600

    ok

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -1,12 +1,40 @@
-import React from 'react'
-import './Footer.css';
+import React from "react";
+import "./Footer.css";
+
+import {
+  TwitterOutlined,
+  FacebookOutlined,
+  LinkedinOutlined,
+} from "@ant-design/icons";
 
 export default function Footer() {
   return (
-    <div className='footerBody'>
-      <div className='footerBox1'>box1</div>
-      <div className='footerBox2'>box2</div>
-      <div className='footerBox3'>box3</div>
+    <div className="footerBody">
+      <div className="footerBox1">box1</div>
+      <div className="footerBox2">box2</div>
+      <div className="footerBox3">
+        <a
+          style={{ textDecoration: "none" }}
+          href="https://www.google.com/"
+          target="_blank"
+        >
+          <TwitterOutlined className="footerIcon" />
+        </a>
+        <a
+          style={{ textDecoration: "none" }}
+          href="https://www.google.com/"
+          target="_blank"
+        >
+          <FacebookOutlined className="footerIcon" />
+        </a>
+        <a
+          style={{ textDecoration: "none" }}
+          href="https://www.google.com/"
+          target="_blank"
+        >
+          <LinkedinOutlined className="footerIcon" />
+        </a>
+      </div>
     </div>
-  )
+  );
 }
