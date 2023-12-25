3cd523285c7e6fa24736e1b492bf3569f0acd199
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2023-12-26 04:48:27 +0600

    ok

--- src/Screen/Portfolio.js
+++ src/Screen/Portfolio.js
@@ -1,8 +1,7 @@
 import React from "react";
 import "../Style/Portfolio.css";
-import { DownloadOutlined } from '@ant-design/icons';
-import { Button,} from 'antd';
-
+import { DownloadOutlined } from "@ant-design/icons";
+import { Button } from "antd";
 
 export default function Portfolio() {
   return (
@@ -10,9 +9,22 @@ export default function Portfolio() {
       <div className="portfolio-Box1">
         <div className="portfolio-Left">
           <h3>Portfolio</h3>
+          <div className="portfolio-Left-Round1">
+            <div className="portfolio-Left-Round2">
+              <div className="portfolio-Left-Round3">
+                <div className="portfolio-Left-Round4"> img</div>
+              </div>
+            </div>
+          </div>
+          <p>BE CREATIVE </p>
         </div>
         <div className="portfolio-Right">
-        <Button className="portfolio-RightButton" type="primary" shape="round" icon={<DownloadOutlined />}>
+          <Button
+            className="portfolio-RightButton"
+            type="primary"
+            shape="round"
+            icon={<DownloadOutlined />}
+          >
             Download CV
           </Button>
         </div>
