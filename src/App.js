8c6cc69b7b65bf8008a5def63f7fda954d515323
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2023-12-26 00:22:34 +0600

    ok

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import './App.css';
-import Home from './Screen/Home.js';
-import Header from './Component/Header.js';
+import "./App.css";
+
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+import Home from "./Screen/Home.js";
+import ErrorPage from './Screen/ErrorPage.js';
+import AboutUs from "./Screen/AboutUs.js";
+import Contact from './Screen/Contact.js';
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <Home/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="*" element={<ErrorPage />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/About-Us" element={<AboutUs />} />
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Screen/Contact.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import '../Style/Contact.css';
+
+export default function Contact() {
+  return (
+    <div>Contact</div>
+  )
+}
