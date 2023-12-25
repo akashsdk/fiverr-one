1efeb0b24d35742beae06f4dbb8d1af2665fc276
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2023-12-26 04:18:14 +0600

    ok

--- src/Screen/Portfolio.js
+++ src/Screen/Portfolio.js
@@ -1,12 +1,20 @@
-import React from 'react'
-import '../Style/Portfolio.css';
+import React from "react";
+import "../Style/Portfolio.css";
 
 export default function Portfolio() {
   return (
-    <div className='portfolioBody'>
-        <div style={{
-            height:'200px'
-        }}>bbb</div>
+    <div className="portfolioBody">
+      <div className="portfolio-Box1">
+        <div className="portfolio-Left">
+          <h3>Portfolio</h3>
+        </div>
+        <div className="portfolio-Right">
+        <h3>Portfolio</h3>
+        </div>
+      </div>
+      <div className="portfolio-Box1-Line"/>
+
+      <div>bbb</div>
     </div>
-  )
+  );
 }
