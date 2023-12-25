ebe6f372351e1c0f1c66393256187eb8074c2c8d
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2023-12-26 01:02:18 +0600

    ok

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -3,6 +3,10 @@ import './Footer.css';
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footerBody'>
+      <div className='footerBox1'>box1</div>
+      <div className='footerBox2'>box2</div>
+      <div className='footerBox3'>box3</div>
+    </div>
   )
 }
