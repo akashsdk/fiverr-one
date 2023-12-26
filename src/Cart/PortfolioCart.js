d0a34f317fef8de8de5d13710d38e088d080b8dd
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2023-12-27 01:15:31 +0600

    ok

--- src/Cart/PortfolioCart.js
+++ src/Cart/PortfolioCart.js
@@ -1,14 +1,20 @@
-import React from 'react'
-import './PortfolioCart.css';
+import React from "react";
+import "./PortfolioCart.css";
 
-import logo from '../Img/on-time.png';
+import logo from "../Img/on-time.png";
+import { Button } from "antd";
 
-export default function PortfolioCart() {
+export default function PortfolioCart(props) {
   return (
-    <div className='PorCartBody'>
-      <img className='PorCartLogo' src={logo} alt=''/>
-      <p className='PorCartText-1'>Client Satisfaction</p>
-      <p className='PorCartText-2'>client Satisfaction client Satisfactionclient Satisfaction</p>
+    <div className="PorCartBody">
+      <img className="PorCartLogo" src={logo} alt="" />
+      <p className="PorCartText-1">Client Satisfaction</p>
+      <p className="PorCartText-2">
+        client Satisfaction client Satisfactionclient Satisfaction
+      </p>
+      <Button className="PorCartButton" type="primary" shape="round">
+        Learn More
+      </Button>
     </div>
-  )
+  );
 }
