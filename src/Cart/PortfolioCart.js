c5f6e517b1d2e3281cd446ebe6ff6a3f9463f235
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2023-12-27 01:04:56 +0600

    ok

--- src/Cart/PortfolioCart.js
+++ src/Cart/PortfolioCart.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import './PortfolioCart.css';
 
+import logo from '../Img/on-time.png';
+
 export default function PortfolioCart() {
   return (
-    <div>Portfolio</div>
+    <div className='PorCartBody'>
+      <img className='PorCartLogo' src={logo} alt=''/>
+      <p className='PorCartText-1'>Client Satisfaction</p>
+      <p className='PorCartText-2'>client Satisfaction client Satisfactionclient Satisfaction</p>
+    </div>
   )
 }
