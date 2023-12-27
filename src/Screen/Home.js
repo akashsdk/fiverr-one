268af23b402c91c1ba1bd4862de69c6f6b72ceca
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2023-12-28 01:58:30 +0600

    ok

--- src/Screen/Home.js
+++ src/Screen/Home.js
@@ -1,9 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../Style/Home.css";
 
 import { Image } from "antd";
 
 export default function Home() {
+  const [activeBox, setActiveBox] = useState(1);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setActiveBox((prevBox) => (prevBox % 3) + 1); // Cycle through boxes 1, 2, 3
+    }, 5000); // Change box after every 5 seconds
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const handleBoxChange = (boxNumber) => {
+    setActiveBox(boxNumber);
+  };
   return (
     <div className="homeBody">
       <div className="home-box1">
@@ -29,11 +42,41 @@ export default function Home() {
             src="https://img.freepik.com/free-vector/web-development-programmer-engineering-coding-website-augmented-reality-interface-screens-developer-project-engineer-programming-software-application-design-cartoon-illustration_107791-3863.jpg?size=626&ext=jpg&ga=GA1.1.1546980028.1703462400&semt=ais"
           />
         </div>
-        
+
         <div className="home-box2-1">
           <p>SERVICES </p>
           <p>What I Do, For You!</p>
-
+          <div className="sliding-box-container">
+            <div className={`box ${activeBox === 1 ? "active" : ""}`}>
+              Box 1
+            </div>
+            <div className={`box ${activeBox === 2 ? "active" : ""}`}>
+              Box 2
+            </div>
+            <div className={`box ${activeBox === 3 ? "active" : ""}`}>
+              Box 3
+            </div>
+            <div className="buttons">
+              <button
+                onClick={() => handleBoxChange(1)}
+                className={activeBox === 1 ? "active-button" : ""}
+              >
+                Button 1
+              </button>
+              <button
+                onClick={() => handleBoxChange(2)}
+                className={activeBox === 2 ? "active-button" : ""}
+              >
+                Button 2
+              </button>
+              <button
+                onClick={() => handleBoxChange(3)}
+                className={activeBox === 3 ? "active-button" : ""}
+              >
+                Button 3
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
