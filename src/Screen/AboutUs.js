610eaf8449f977a6484a33537974346753165f32
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2023-12-29 00:05:59 +0600

    ok

--- src/Screen/AboutUs.js
+++ src/Screen/AboutUs.js
@@ -7,31 +7,6 @@ export default function AboutUs() {
   
   return (
     <div className='aboutBody'>
-      <div className='aboutBox'>
-        <div className='aboutBox-1'>
-          <div className='aboutBox-2'>Name:</div>
-          <div className='aboutBox-3'>
-            <input className='aboutBox-input' placeholder='Name'/>
-          </div>
-        </div>
-
-        <div className='aboutBox-1'>
-          <div className='aboutBox-2'>Email:</div>
-          <div className='aboutBox-3'>Tasnim@ggg.com</div>
-        </div>
-
-        <div className='aboutBox-1'>
-          <div className='aboutBox-2'>Phone:</div>
-          <div className='aboutBox-3'>34567898765</div>
-        </div>
-
-        <div className='aboutBox-1'>
-          <div className='aboutBox-4'>Text:</div>
-          <div className='aboutBox-5'>34hyghg765</div>
-        </div>
-
-        <button className='aboutBox-Button'>Submit</button>
-      </div>
     </div>
   )
 }
